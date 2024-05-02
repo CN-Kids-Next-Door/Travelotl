428c35fd9c76d682c5331cab1bf9867be13d3c86
Elizabeth Mazorra Vidal <emazorra1@gmail.com>
2024-05-01 22:54:03 -0400

    working on http req tests

--- __tests__/db/UserController.test.js
+++ __tests__/db/UserController.test.js
@@ -2,6 +2,9 @@ jest.mock('./mockModel');
 
 const { getUsersController } = require('./UserController');
 const db = require('./mockModel');
+const request = require('supertest');
+const app = require('../../server/server.js');
+
 
 beforeAll(done => {
     done()
@@ -37,8 +40,26 @@ describe('getUsersController', () => {
         expect(res.status).toHaveBeenLastCalledWith(500);
         expect(res.json).toHaveBeenCalledWith({ error: 'Failed to fetch users' });
     });
+})
 
+//http request tests
+describe('POST to /auth/login', () => {
     //it should be able to login in a user by comparing info
+    it('should register a user to the database', async () => {
+        const userData = [{"userInfo": { "email": "test@test.com", "username": "iamuser", "password": "password", "firstName": "name", "lastName": "last"}, "token": "NOTOKEN"}];
+
+        const response = await request(app)
+            .post('/auth/login')
+            .send(userData);
+
+
+        expect(response.status).toBe(201);
+
+        const userDataInDB = await db.query('SELECT * FROM users WHERE username = $1;', [userData.userInfo.username]);
+        expect(usersInDatabase.rows.length).toBe(1);
+        expect(usersInDatabase.rows[0].userInfo.email).toBe(userData.userInfo.email);
+    })
+})
+    
 
     //it should be able to register a user by adding to the database
-})
