b3b2439d266ad3ddb2b20ad20238d66c52ab70d7
Elizabeth Mazorra Vidal <emazorra1@gmail.com>
2024-05-04 12:41:23 -0400

    stop working on this branch, moving to adams

--- client/components/ItinerariesWelcome/ItinerariesWelcome.jsx
+++ client/components/ItinerariesWelcome/ItinerariesWelcome.jsx
@@ -32,7 +32,8 @@ export default function Example() {
     imageUrl: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   };
 
-  const userId = 26; // Example userId, adjust according to your app logic
+  const userId = useSelector((state) => state.authState.userInfo.userId);
+  console.log('this is userID in itnry welcome:', userId);
   const { data: itineraries, isLoading, isError, error } = useGetItinerariesQuery({ userId }, { skip: !userId });
 
   return (
